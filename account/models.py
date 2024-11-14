barnslig/geheimvz

commit 106482eba4a69d9f09d6933988c00cc5e03a8687

refactor: Move privacy settings into PrivacySettings model

account/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from friendship.models import Friend
 
 User = get_user_model()
 
@@ -103,6 +104,64 @@ class NotificationSettings(models.Model):
         default=True,
         verbose_name=_("On new group invitation"),
     )
+
+    def __str__(self):
+        return f"Settings for {self.owner.display_name}"
+
+
+class PrivacySettings(models.Model):
+    class PrivacyChoices(models.TextChoices):
+        EVERYONE = "E", _("Everyone logged in")
+        FRIENDS = "F", _("Only my friends")
+
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="privacy_settings",
+    )
+
+    can_see_profile = models.CharField(
+        max_length=1,
+        choices=PrivacyChoices,
+        default=PrivacyChoices.FRIENDS,
+        verbose_name=_("Who can see your page?"),
+    )
+    can_send_messages = models.CharField(
+        max_length=1,
+        choices=PrivacyChoices,
+        default=PrivacyChoices.FRIENDS,
+        verbose_name=_("Who can send you messages?"),
+    )
+
+    def get_can_see_profile(self, user):
+        if self.can_see_profile == PrivacySettings.PrivacyChoices.EVERYONE:
+            return True
+
+        if user == self:
+            return True
+
+        if Friend.objects.are_friends(self.owner, user):
+            return True
+
+        if user.is_superuser:
+            return True
+
+        return False
+
+    def get_can_send_messages(self, user):
+        if self.can_send_messages == PrivacySettings.PrivacyChoices.EVERYONE:
+            return True
+
+        if Friend.objects.are_friends(self.owner, user):
+            return True
+
+        if user.friendship_requests_sent.filter(to_user=user).exists():
+            return True
+
+        if user.is_superuser:
+            return True
+
+        return False
 
     def __str__(self):
         return f"Settings for {self.owner.display_name}"

core/models.py
@@ -101,24 +101,6 @@ class User(AbstractUser):
     )
     i_wish_for = models.TextField(null=True, blank=True, verbose_name=_("I wish for"))
 
-    # privacy settings
-    class PrivacyChoices(models.TextChoices):
-        EVERYONE = "E", _("Everyone logged in")
-        FRIENDS = "F", _("Only my friends")
-
-    can_see_profile = models.CharField(
-        max_length=1,
-        choices=PrivacyChoices,
-        default=PrivacyChoices.FRIENDS,
-        verbose_name=_("Who can see your page?"),
-    )
-    can_send_messages = models.CharField(
-        max_length=1,
-        choices=PrivacyChoices,
-        default=PrivacyChoices.FRIENDS,
-        verbose_name=_("Who can send you messages?"),
-    )
-
     @property
     def display_name(self):
         if self.full_name and len(self.full_name) > 0:
@@ -162,36 +144,6 @@ class User(AbstractUser):
         )
 
         return fof
-
-    def get_can_see_profile(self, user):
-        if self.can_see_profile == User.PrivacyChoices.EVERYONE:
-            return True
-
-        if user == self:
-            return True
-
-        if Friend.objects.are_friends(self, user):
-            return True
-
-        if user.is_superuser:
-            return True
-
-        return False
-
-    def get_can_send_messages(self, user):
-        if self.can_send_messages == User.PrivacyChoices.EVERYONE:
-            return True
-
-        if Friend.objects.are_friends(self, user):
-            return True
-
-        if user.friendship_requests_sent.filter(to_user=user).exists():
-            return True
-
-        if user.is_superuser:
-            return True
-
-        return False
 
     def get_absolute_url(self):
         return reverse("profile", kwargs={"id": self.id})

account/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth import forms as auth_forms
 from django.utils.translation import gettext_lazy as _, gettext
 
-from .models import AppearanceSettings, NotificationSettings
+from .models import AppearanceSettings, NotificationSettings, PrivacySettings
 from core.models import User
 from core.forms import RightColumn
 
@@ -77,7 +77,7 @@ class AppearanceForm(forms.ModelForm):
 
 class PrivacyForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = PrivacySettings
         fields = [
             "can_see_profile",
             "can_send_messages",

account/migrations/0003_privacysettings.py
@@ -1,0 +1,53 @@
+# Generated by Django 5.1.2 on 2024-11-14 20:19
+
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0002_appearancesettings'),
+        ("core", "0004_alter_user_image"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PrivacySettings',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('can_see_profile', models.CharField(choices=[('E', 'Everyone logged in'), ('F', 'Only my friends')], default='F', max_length=1, verbose_name='Who can see your page?')),
+                ('can_send_messages', models.CharField(choices=[('E', 'Everyone logged in'), ('F', 'Only my friends')], default='F', max_length=1, verbose_name='Who can send you messages?')),
+                ('owner', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='privacy_settings', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.RunSQL(
+            """
+            INSERT INTO account_privacysettings (
+                owner_id,
+                can_see_profile,
+                can_send_messages
+            )
+            SELECT
+                id AS owner_id,
+                can_see_profile,
+                can_send_messages
+            FROM
+                core_user
+            """,
+            reverse_sql="""
+                INSERT INTO core_user (
+                    can_see_profile,
+                    can_send_messages
+                ) SELECT
+                    can_see_profile,
+                    can_send_messages
+                FROM
+                    account_privacysettings
+                WHERE
+                    core_user.id = account_privacysettings.owner_id
+            """,
+        ),
+    ]

account/views.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth import views as auth_views
 
-from .models import AppearanceSettings, NotificationSettings
+from .models import AppearanceSettings, NotificationSettings, PrivacySettings
 from core.components.tabs.tabs import TabsMixin
 from core.models import User
 
@@ -37,23 +37,6 @@ tabs = {
         "label": _("Privacy"),
     },
 }
-
-
-class NotificationSettingsUpdateView(
-    TabsMixin,
-    LoginRequiredMixin,
-    SuccessMessageMixin,
-    UpdateView,
-):
-    form_class = NotificationSettingsForm
-    model = NotificationSettings
-    tabs = tabs
-    tab_current = "notifications"
-    success_message = _("Notification settings successfully updated")
-    success_url = reverse_lazy("account-notifications")
-
-    def get_object(self):
-        return self.request.user.notification_settings
 
 
 class ProfileUpdateView(
@@ -91,6 +74,23 @@ class AppearanceUpdateView(
         return self.request.user.appearance_settings
 
 
+class NotificationSettingsUpdateView(
+    TabsMixin,
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    UpdateView,
+):
+    form_class = NotificationSettingsForm
+    model = NotificationSettings
+    tabs = tabs
+    tab_current = "notifications"
+    success_message = _("Notification settings successfully updated")
+    success_url = reverse_lazy("account-notifications")
+
+    def get_object(self):
+        return self.request.user.notification_settings
+
+
 class PrivacyUpdateView(
     TabsMixin,
     LoginRequiredMixin,
@@ -98,14 +98,15 @@ class PrivacyUpdateView(
     UpdateView,
 ):
     form_class = PrivacyForm
-    model = User
+    model = PrivacySettings
     success_message = _("Profile successfully updated")
+    success_url = reverse_lazy("account-privacy")
     template_name = "account/privacy_form.html"
     tabs = tabs
     tab_current = "privacy"
 
     def get_object(self):
-        return self.request.user
+        return self.request.user.privacy_settings
 
 
 class UserDeleteView(LoginRequiredMixin, DeleteView):

core/components/post/post.py
@@ -13,7 +13,9 @@ class Post(Component):
             current_user = request.user
 
         return {
-            "can_send_messages": created_by.get_can_send_messages(current_user),
+            "can_send_messages": created_by.privacy_settings.get_can_send_messages(
+                current_user
+            ),
             "created_at": created_at,
             "created_by": created_by,
             "post": post,

core/migrations/0005_remove_user_can_see_profile_and_more.py
@@ -1,0 +1,23 @@
+# Generated by Django 5.1.2 on 2024-11-14 20:24
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_alter_user_image'),
+        ("account", "0003_privacysettings"),
+
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='user',
+            name='can_see_profile',
+        ),
+        migrations.RemoveField(
+            model_name='user',
+            name='can_send_messages',
+        ),
+    ]

core/views.py
@@ -60,8 +60,8 @@ def profile(request: HttpRequest, id: str):
 
         return None
 
-    can_see_profile = user.get_can_see_profile(request.user)
-    can_send_messages = user.get_can_send_messages(request.user)
+    can_see_profile = user.privacy_settings.get_can_see_profile(request.user)
+    can_send_messages = user.privacy_settings.get_can_send_messages(request.user)
 
     are_friends = Friend.objects.are_friends(request.user, user)
 

commit f1d26e53466d343e41838df8ca71730413e95d9c

fix: Let users see their own page again
Bug introduced in 106482eba4a69d9f09d6933988c00cc5e03a8687

account/models.py
@@ -138,7 +138,7 @@ class PrivacySettings(models.Model):
         if self.can_see_profile == PrivacySettings.PrivacyChoices.EVERYONE:
             return True
 
-        if user == self:
+        if user == self.owner:
             return True
 
         if Friend.objects.are_friends(self.owner, user):